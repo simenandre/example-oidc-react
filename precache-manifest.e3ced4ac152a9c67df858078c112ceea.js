b0617abc59c8b05e5787e88ec5c28f4d9430d36c
Simen A. W. Olsen <cobraz@cobraz.no>
2020-04-09 21:52:53 +0200

    Updates

---
+++ precache-manifest.e3ced4ac152a9c67df858078c112ceea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b00728393969c9fc36d44cca4f5be9",
+    "url": "/example-react-oidc/index.html"
+  },
+  {
+    "revision": "225a32001cf99787c94c",
+    "url": "/example-react-oidc/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fff63f8fa4c3c7a6e734",
+    "url": "/example-react-oidc/static/js/2.6adb8882.chunk.js"
+  },
+  {
+    "revision": "4d1745f45a6e99543a6cd94a263b10c5",
+    "url": "/example-react-oidc/static/js/2.6adb8882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225a32001cf99787c94c",
+    "url": "/example-react-oidc/static/js/main.725b7eaa.chunk.js"
+  },
+  {
+    "revision": "bcb1932fd4f7cff0779e",
+    "url": "/example-react-oidc/static/js/runtime-main.5859c816.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/example-react-oidc/static/media/logo.5d5d9eef.svg"
+  }
+]);
